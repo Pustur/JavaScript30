4f740c49c87855b1bcc3fbac022d40cc02f30e13
Loris Bettazza <loris.bettazza@gmail.com>
2018-03-30 04:01:28 +0200

    Prepare challenge 09

---
+++ 09 - Dev Tools Domination/script.js
@@ -0,0 +1,36 @@
+// Functions
+function makeGreen() {
+  paragraph.style.color = '#BADA55';
+  paragraph.style.fontSize = '50px';
+}
+
+// DOM elements
+const paragraph = document.querySelector('p');
+
+// Variables
+const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+
+// Body
+// Regular
+
+// Interpolated
+
+// Styled
+
+// Warning!
+
+// Error :|
+
+// Info
+
+// Testing
+
+// Clearing
+
+// Viewing DOM Elements
+
+// Grouping together
+
+// Counting
+
+// Timing
