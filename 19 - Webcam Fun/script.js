4cf84d79bf9d46e1a489f50051b9518c7add7f86
Loris Bettazza <loris.bettazza@gmail.com>
2018-04-01 22:44:10 +0200

    Prepare challege 19

--- 19 - Webcam Fun/script.js
+++ 19 - Webcam Fun/script.js
@@ -1,5 +1,9 @@
+// DOM elements
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+const photoButton = document.querySelector('.take-photo');
+
+// Variables
+const ctx = canvas.getContext('2d');
