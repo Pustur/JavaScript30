4ec8f4a558a40d8558d130dff1558ca476ff9cec
Loris Bettazza <loris.bettazza@gmail.com>
2018-04-06 03:19:24 +0200

    Finish challenge 23

--- 23 - Speech Synthesis/script.js
+++ 23 - Speech Synthesis/script.js
@@ -1,9 +1,92 @@
+// Functions
+function loadVoices(e) {
+  const { currentTarget } = e;
+
+  voices = currentTarget.getVoices();
+
+  const voicesByLang = voices.sort(sortByLangThenName).reduce((obj, curr) => {
+    const key = curr.lang.slice(0, 2);
+
+    return {
+      ...obj,
+      [key]: obj[key] ? obj[key].concat(curr) : [].concat(curr),
+    };
+  }, {});
+
+  renderSelect(voicesByLang);
+  utterance.voice = voices.find(voice => voice.name === defaultVoice);
+}
+
+function sortByLangThenName(a, b) {
+  const langComparison = a.lang.slice(0, 2).localeCompare(b.lang.slice(0, 2));
+
+  if (langComparison === 0) return a.name.localeCompare(b.name);
+
+  return langComparison;
+}
+
+function renderSelect(voicesByLang) {
+  voicesDropdown.innerHTML = Object.entries(voicesByLang)
+    .map(entry => {
+      const [langGroup, voices] = entry;
+      const optionsHtml = voices
+        .map(voice => {
+          const { name, lang } = voice;
+          const selected = name === defaultVoice ? 'selected' : '';
+
+          return `<option value="${name}" ${selected}>${lang} (${name})</option>`;
+        })
+        .join('');
+
+      return `<optgroup label="${langGroup}">${optionsHtml}</optgroup>`;
+    })
+    .join('');
+}
+
+function voiceChangeHandler(e) {
+  const { currentTarget: { value } } = e;
+
+  utterance.voice = voices.find(voice => voice.name === value);
+  stop();
+  speak();
+}
+
+function optionChangeHandler(e) {
+  const { currentTarget: { value, name } } = e;
+
+  utterance[name] = value;
+  stop();
+  speak();
+}
+
+function speak() {
+  if (!speechSynthesis.speaking) speechSynthesis.speak(utterance);
+}
+
+function stop() {
+  speechSynthesis.cancel();
+}
+
 // DOM elements
 const voicesDropdown = document.querySelector('[name="voice"]');
-const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector('#speak');
-const stopButton = document.querySelector('#stop');
+const textarea = document.querySelector('[name="text"]');
+const options = document.querySelectorAll('[type="range"]');
+const speakButton = document.querySelector('.speak');
+const stopButton = document.querySelector('.stop');
 
 // Variables
-const msg = new SpeechSynthesisUtterance();
+const utterance = new SpeechSynthesisUtterance();
+const defaultVoice = 'Alex';
 let voices = [];
+
+// Events
+speechSynthesis.addEventListener('voiceschanged', loadVoices, { once: true });
+voicesDropdown.addEventListener('change', voiceChangeHandler);
+speakButton.addEventListener('click', speak);
+stopButton.addEventListener('click', stop);
+[textarea, ...options].forEach(option =>
+  option.addEventListener('change', optionChangeHandler),
+);
+
+// Body
+utterance.text = textarea.value;
